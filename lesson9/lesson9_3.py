dc60e83a34043f70c68cf863cef97e0a1b335cf8
shensh1 <cynthia.shen.ssh@gmail.com>
2025-08-03 06:53:42 +0000

    1st

---
+++ lesson9/lesson9_3.py
@@ -0,0 +1,26 @@
+#請幫我自訂一個function
+#連線至postgres DB
+#建立連線環境參數的樣版
+import psycopg2
+
+def create_connection():
+    conn = psycopg2.connect(
+        host = "host.docker.interal",
+        dabase = "postgres"
+        user="postgres",
+        password = "sh910418",
+        port="5432"
+    )
+    return conn
+
+def main():
+    conn = create_connection()
+    if conn:
+        print("成功連接到資料庫!")
+        conn.close()
+    else:
+        print("無法連線到資料庫!")
+
+
+if  __name__ == "__main_":
+    main()
