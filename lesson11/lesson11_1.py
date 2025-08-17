f80efbfe3be3ec94871f4a3a40a35939e6772ee1
shensh1 <cynthia.shen.ssh@gmail.com>
2025-08-17 06:02:23 +0000

    TEST

--- lesson11/lesson11_1.py
+++ lesson11/lesson11_1.py
@@ -5,15 +5,27 @@ st.sidebar.title("台鐵車站資訊")
 st.sidebar.header("2023年各站進出人數")
 st.subheader("進出站人數顯示區")
 
-@st.cache_resource
+@st.cache_data
 def get_stations():
     """取得車站資料"""
     return datasource.get_stations_names()
 
 stations = get_stations()
-station = st.sidebar.selectbox(
-    "請選擇車站",
-    stations,
-)
+if stations is None:
+    st.error("無法取得車站資料，請稍後再試。")
+    st.stop()
+
+
+common_stations = ['臺北','桃園','新竹','台中','臺南','高雄','其它']
+
+choice = st.sidebar.radio("快速選擇常用車站", common_stations)
+
+if choice == "其它":
+    station = st.sidebar.selectbox(
+        "請選擇車站",
+        stations,
+    )
+else:
+    station = choice
 
 st.write("您選擇的車站:", station)
